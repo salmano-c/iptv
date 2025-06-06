566a8d5aa21608a2da109b4700260e46d6104d5c
Yassine Elbounouhi <157987251+salmano-c@users.noreply.github.com>
2025-06-06 12:31:58 +0100

    Update main.tsx

--- cinematic-neon-streams-main/src/main.tsx
+++ cinematic-neon-streams-main/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
 createRoot(document.getElementById("root")!).render(<App />);
